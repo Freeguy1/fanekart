6da4288c36f4433d92392ab5451c75bafe88601a
Jan Kristian Jensen <jensen72@gmail.com>
2016-03-18 14:29:04 +0100

    La til selve webapplikasjonen
    La til selve webapplikasjonen (flask_app.py, loggGPs.py)

---
+++ flask_app.py
@@ -0,0 +1,47 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, request, url_for, render_template, make_response, abort
+import loggGps
+
+# Directory where we write (and read!) our geojson-data
+dataDir = '/home/ssm1937/mysite/static/gps/'
+
+
+app = Flask(__name__)
+app.secret_key = 'This is really unique and secret'
+
+@app.route('/getfile/<filename>')
+def getFile(filename):
+    filename = dataDir + filename
+
+    try:
+        with open( filename) as file:
+            blob = file.read()
+    except:
+        abort(404)
+        pass
+
+    r = make_response(blob)
+    r.mimetype='application/json'
+    r.headers['Access-Control-Allow-Origin'] = '*'
+
+    return r
+
+
+@app.route('/gps/mypos/<gpsId>/')
+def getpos(gpsId=''):
+    lat = request.args.get('lat', '')
+    lon = request.args.get('lon', '')
+
+    ok = loggGps.loggGps( str(lat), str(lon), str(gpsId))
+
+    return( ok + "<br>gps ID" + str(gpsId) + "<br>\n" +
+            'lat' + str(lat) + "<br>\n" +
+            "lon" + str(lon) )
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
